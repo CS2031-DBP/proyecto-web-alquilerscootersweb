1d20f90bc2966ca69e420a3eb91ef0f4e2bf710b
Luis23345432 <luis.lopez.c@utec.edu.pe>
2024-07-07 22:13:51 -0500

    siuu

--- alquilerscootersweb_proyectoweb/src/components/ScooterMap.jsx
+++ alquilerscootersweb_proyectoweb/src/components/ScooterMap.jsx
@@ -4,7 +4,7 @@ import { fetchScooters } from '../services/api'; // Asegúrate de que la ruta al
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import PropTypes from 'prop-types';
-import './ScooterMap.css'; // Asegúrate de tener un archivo CSS para los estiloss
+import './ScooterMap.css'; // Asegúrate de tener un archivo CSS para los estilosss
 
 // Icono personalizado para scooterss
 const scooterIcon = new L.Icon({
